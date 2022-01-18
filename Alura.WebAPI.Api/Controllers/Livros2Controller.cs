3d23a8f4803530952bb68ffa1ac86e393df14985
William Fernandes Magalhaes <williamfmagalhaes@hotmail.com>
2022-01-18 14:45:40 -0300

    ordenacao mais filtro

--- Alura.WebAPI.Api/Controllers/Livros2Controller.cs
+++ Alura.WebAPI.Api/Controllers/Livros2Controller.cs
@@ -1,5 +1,6 @@
 ﻿using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
+using Alura.WebAPI.Api.Modelos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -23,9 +24,16 @@ namespace Alura.ListaLeitura.Api.Controllers
         }
 
         [HttpGet]
-        public IActionResult ListaDeLivros()
+        public IActionResult ListaDeLivros(
+                [FromQuery] LivroFiltro filtro,
+                [FromQuery] LivroOrdem ordem,
+                [FromQuery] LivroPaginacao paginacao)
         {
-            var lista = _repo.All.Select(l => l.ToApi()).ToList();
+            var lista = _repo.All
+                .AplicaFiltro(filtro)
+                .AplicaOrdem(ordem)
+                .Select(l => l.ToApi())
+                .ToList();
             return Ok(lista);
         }
 
---
+++ Alura.WebAPI.Api/Modelos/LivroPaginacao.cs
@@ -0,0 +1,26 @@
+﻿using Alura.ListaLeitura.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.Api.Modelos
+{
+    public class LivroPaginacao
+    {
+        public int Pagina { get; set; }
+        public int Tamanho { get; set; }
+    }
+
+    public class LivroPaginado
+    {
+        public int Total { get; set; }
+
+        public int TamanhoPagina { get; set; }
+        public int NumeroPagina { get; set; }
+        public int TotalPaginas { get; set; }
+        public IList<LivroApi> Resultado { get; set; }
+        public string Anterior { get; set; }
+        public string ProximaPagina { get; set; }
+    }
+}
